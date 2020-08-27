3b3b4783276af6ba52c5cc1266a1fd0372a059d2
antonio <acobos@vectoritcgroup.com>
2020-08-27 13:14:36 +0200

    Se ha creado la entidad que apunta a la tabla de la base de datos en memoria, que se puede consultar mientras esta la aplicacion arrancada, el repositorio para llamar a base de datos, y los datos que carga h2, que es un archivo que se llama data.sql y busca H2 al arrancar

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,14 +1,44 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.DemoEntity;
+import com.example.demo.repository.DemoRepository;
+
 @RestController
+@RequestMapping("/demo")
 public class DemoController {
 
+	@Autowired
+	private DemoRepository demoRepository;
+	
 	@GetMapping(path = "/demo")
 	public String getDemo() {
+	
 		return "Buenas";
+	}
+	
+	@GetMapping(path = "/{demoId}")
+	public ResponseEntity<DemoEntity> getDemo(@PathVariable(name = "demoId") Integer id) {
+		ResponseEntity<DemoEntity> response = null;
+		
+		Optional<DemoEntity> demoEntity = demoRepository.findById(id);
+		
+		if(demoEntity.isPresent()) {
+			response = new ResponseEntity<DemoEntity>(demoEntity.get(),HttpStatus.OK);
+		}else {
+			response = new ResponseEntity<DemoEntity>(demoEntity.get(),HttpStatus.NOT_FOUND);
+		}
+		
+		return response;
 	}
 	
 }
